22a4e4068e186a519829ff8389b65beea85ddc64
Marc Abillama <mabillama@gmail.com>
2022-02-11 08:28:13 +0100

    Error and exception handling. Overriding errorHandler and adding express-async-handler to login and user register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,30 @@
-const registerUser = (req, res) => {
-  res.send("Register Route");
-};
+const asyncHandler = require("express-async-handler");
 
-const loginUser = (req, res) => {
+// @desc    Register a new user
+// @route   /api/users
+// @access  Public
+const registerUser = asynHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+
+  res.send("Register Route");
+});
+
+// @desc    Login user
+// @route   /api/users/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
   res.send("Login Route");
-};
+});
 
 module.exports = {
   registerUser,
   loginUser,
 };
+
+// adding data in the body to send to the register and login routes
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Loads piece of middleware related to capturing data information
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // to create a route with express. This will create a response to an http request.
 app.get("/", (req, res) => {
@@ -12,5 +17,7 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
