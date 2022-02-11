1a39491927bda20283e97947abc1caf78db061c9
Marc Abillama <mabillama@gmail.com>
2022-02-11 07:27:59 +0100

    Basic backend creation using Express

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// to create a route with express. This will create a response to an http request.
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Support Desk API" });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
